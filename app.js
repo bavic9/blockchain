718146b4b9744b5e825b5da4a8969e3d065a7cee
Bavic <bavic97@gmail.com>
2024-03-09 15:51:05 +0100

    finished

---
+++ app.js
@@ -0,0 +1,28 @@
+// const container = document.getElementById('container');
+// const registerBtn = document.getElementById('register');
+// const loginBtn = document.getElementById('login');
+
+// registerBtn.addEventListener('click', () => {
+//     container.classList.add("active");
+// });
+
+// loginBtn.addEventListener('click', () => {
+//     container.classList.remove("active");
+// });
+
+
+let tabs = document.querySelectorAll(".tabs h3");
+let tabContents = document.querySelectorAll(".tab-content > div");
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+        tabContents.forEach((content) => {
+            content.classList.remove("active");
+        });
+        tabs.forEach((tab) => {
+            tab.classList.remove("active");
+        });
+        tabContents[index].classList.add("active");
+        tabs[index].classList.add("active");
+    });
+});
